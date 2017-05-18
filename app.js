2c200f64562d9fbed011654fb36f12ec383bce58
Simon-2021 <mengsir10@163.com>
2017-05-18 19:54:22 +0800

    修改端口号

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app = http.createServer(function(req, res){
     console.log('get the reqest.')
     res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<head><meta charset="utf-8"/></head>');
     res.write('<h1>你是不是傻</h1>');
     res.end();
 }).listen(3002,'172.18.77.43',function(){
