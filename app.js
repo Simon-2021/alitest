84c918e9debd08d1c1a5f2d89eab143746f449ab
Simon-2021 <mengsir10@163.com>
2017-05-18 19:45:23 +0800

    修改端口号

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var http = require('http');
 var app = http.createServer(function(req, res){
     console.log('get the reqest.')
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World</p>');
+    res.write('<h1>你是不是傻</h1>');
+    res.end();
 }).listen(3002,'119.23.254.236',function(){
     console.log('Server is created at port 3002.');
 });
