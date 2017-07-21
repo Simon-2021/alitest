d533beb92f5ce598b12fefb69e8bd05c402d77df
Simon-2021 <mengsir10@163.com>
2017-07-21 16:09:04 +0800

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = http.createServer(function(req, res){
     console.log('get the reqest.')
     res.writeHead(200,{'Content-Type':'text/html'});
     res.write('<head><meta charset="utf-8"/></head>');
-    res.write('<h1>你是不是傻</h1>');
+    res.write('<h1>你是不是傻子</h1>');
     res.end();
 }).listen(3002,'172.18.77.43',function(){
     console.log('Server is created at port 3002.');
